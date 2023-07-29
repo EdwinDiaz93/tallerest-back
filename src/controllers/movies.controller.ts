523ef8d14656c0e10d800f6c141714d6de406f01
EdwinDiaz93 <josephph1993@gmail.com>
2023-07-29 11:23:26 -0600

    added pagination to get movies

--- src/controllers/movies.controller.ts
+++ src/controllers/movies.controller.ts
@@ -2,21 +2,45 @@ import { Request, Response } from 'express';
 import Movie from '../models/movies.model';
 
 export class MoviesController {
+
+  static async getMovies(req: Request, res: Response) {
+    try {
+
+      const page: number = +req.query.page || 1;
+      const limit: number = +req.query.page || 10;
+
+      const offset = (page - 1) * limit;
+      const { count, rows } = await Movie.findAndCountAll({
+        order: [
+          ['id', 'DESC'],
+        ],
+        offset,
+        limit,
+      });
+
+      const totalPages = Math.ceil(count / limit);
+
+      const data = {
+        movies: rows,
+        totalRows: count,
+        currentPage: page,
+        firstPage: 1,
+        lastPage: totalPages,
+        totalPages: totalPages,
+      }
+
+      return res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json({ error: 'Error al crear la película' });
+    }
+  };
+
   static async createMovie(req: Request, res: Response) {
     try {
       return res.status(200).json({
         ok: true,
         msg: 'created'
       });
-    } catch (error) {
-      res.status(500).json({ error: 'Error al crear la película' });
-    }
-  };
-
-  static async getMovies(req: Request, res: Response) {
-    try {
-      const movies = await Movie.findAll();
-      return res.status(200).json(movies);
     } catch (error) {
       res.status(500).json({ error: 'Error al crear la película' });
     }
