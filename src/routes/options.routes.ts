835751dfc404b01b90b6a23d26c0d88a93126f2a
EdwinDiaz93 <josephph1993@gmail.com>
2023-07-29 22:23:14 -0600

    added refresh token endpoints

--- src/routes/options.routes.ts
+++ src/routes/options.routes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import { Roles } from '../constants';
 import { OptionsController } from '../controllers';
+import { verifyRoles, verifyToken } from '../middlewares';
 
 
 const optionsRoutes = express.Router();
 
-optionsRoutes.get('/', OptionsController.getOptions);
+optionsRoutes.get('/', [verifyToken, verifyRoles(Roles.ADMIN)], OptionsController.getOptions);
 
 export default optionsRoutes;
